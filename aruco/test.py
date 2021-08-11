11b131b92f213df834cac49caf84640c26b87928
Ralf Puchert <ralf.puchert@camunda.com>
2021-08-11 17:23:33 +0200

    impl(backend): create detection data structure and test

--- aruco/test.py
+++ aruco/test.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 
-cap = cv2.VideoCapture(4)
+cap = cv2.VideoCapture(0)
+
+ids_to_elements = {0: "StartEvent", 1: "ServiceTask", 2: "EndEvent"}
 
 while(True):
     # Capture frame-by-frame
@@ -10,15 +12,25 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-    parameters =  aruco.DetectorParameters_create()
+    parameters = aruco.DetectorParameters_create()
 
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-    print(corners, ids)
-
-    gray = aruco.drawDetectedMarkers(gray, corners)
+    bpmn_snapshort = []
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+    if len(corners) > 0:
+        gray = aruco.drawDetectedMarkers(frame, corners)
+        for id in ids.flatten():
+            current_element = {"id": id, "type": ids_to_elements[id], "corners": corners[0][0]};
+            bpmn_snapshort.append(current_element)
+            # draw also in image
+            elm = current_element
+            x = elm['corners'][0][0]
+            y = elm['corners'][0][1]
+            image = cv2.putText(frame, elm['type'], (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX,
+                            1, (255, 0, 0), 2, cv2.LINE_AA)
+        print(bpmn_snapshort)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('BPMNKlotz', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
