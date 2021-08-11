0893bcf2bc5bdb5cbd36b2dde45ffbf71f124957
Ralf Puchert <ralf.puchert@camunda.com>
2021-08-11 17:22:35 +0200

    impl(backend): add demo websocket server and client

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,17 @@
-print("TODO: Implement a websocket server to stream recognized aruco data")
+import asyncio
+import websockets
+
+
+async def hello(websocket, path):
+  name = await websocket.recv()
+  print(f"< {name}")
+
+  greeting = f"Hello {name}!"
+
+  await websocket.send(greeting)
+  print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
