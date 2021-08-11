972fb01b10fa05cac29f6db641cf33a92fe864a7
Patrick Dehn <patrick.dehn@camunda.com>
2021-08-11 15:17:19 +0200

    add bpmn modeling

--- modeler-plugin/dist/client.js
+++ modeler-plugin/dist/client.js
@@ -96,50 +96,55 @@
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return BpmnKlotzService; });
-/**
- * A bpmn-js service that provides the actual plug-in feature.
- *
- * Checkout the bpmn-js examples to learn about its capabilities
- * and the extension points it offers:
- *
- * https://github.com/bpmn-io/bpmn-js-examples
- */
-
-
-
-const createStartEvent = (canvas, modeling) => {
-  const parent = canvas.getRootElement();
-  modeling.createShape({ type: "bpmn:Task" }, {x: 200, y: 100}, parent);
+const ELEMENTS = {
+  0: 'bpmn:StartEvent',
+  1: 'bpmn:Task',
+  2: 'bpmn:EndEvent'
 }
 
-function BpmnKlotzService(modeling, canvas) {
-  const arucoCoord = [[[[229., 409.],
-    [251., 413.],
-    [245., 437.],
-    [222., 432.]]], [[[445., 321.],
-    [467., 325.],
-    [465., 346.],
-    [443., 342.]]],[[[100., 291.],
-    [122., 294.],
-    [115., 315.],
-    [ 93., 311.]]],]
-    
+function BpmnKlotzService(modeling, elementRegistry, canvas) {
+  const arucoCoord = [
+    [
+        [229., 409.],
+        [251., 413.],
+        [245., 437.],
+        [222., 432.]
+    ], [
+        [445., 321.],
+        [467., 325.],
+        [465., 346.],
+        [443., 342.]
+    ], [
+        [100., 291.],
+        [122., 294.],
+        [115., 315.],
+        [ 93., 311.]
+    ]
+  ]
   const arucoIds =  [1,2,0]
 
-  // eventBus.on('diagram.init', function() {
-  //   createStartEvent(canvas, elementFactory, modeling);
-  // });
-
   document.addEventListener('keydown', (event) => {
-      if (event.key === 'k') {
-      createStartEvent(canvas, modeling);
+    if (event.key === 'k') {
+      // remove all elements
+      const allElements = elementRegistry.getAll();
+      modeling.removeElements(allElements.filter(element => element.type !== "bpmn:Process"));
+
+      // add elements
+      const parent = canvas.getRootElement();
+      arucoCoord.forEach((element, index) => {
+        const type = ELEMENTS[arucoIds[index]];
+        // x/y coords of upper left corner
+        const x = element[0][0];
+        const y = element[0][1];
+        modeling.createShape({ type }, {x, y}, parent);
+      }) 
     }
   })
-
 }
 
 BpmnKlotzService.$inject = [
   'modeling',
+  'elementRegistry',
   'canvas',
 ];
 
