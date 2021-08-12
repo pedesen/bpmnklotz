761347f84afb92285c3c1364f95763cd4ed39e9c
Ralf Puchert <ralf.puchert@camunda.com>
2021-08-12 10:12:06 +0200

    impl(backend): add testdata and websocket browser client

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,67 @@
 import asyncio
+
 import websockets
 
 
+bpmn_snapshot_data = """
+[
+  {
+   'id': 0, 
+   'type': 'StartEvent', 
+   'corners': [
+       [414, 505],
+       [459, 503],
+       [462, 549],
+       [416, 550]
+    ]
+  }, 
+  {
+   'id': 1, 
+   'type': 'ServiceTask', 
+   'corners': [
+       [414, 505],
+       [459, 503],
+       [462, 549],
+       [416, 550]
+    ]
+  }, 
+  {
+   'id': 2, 
+   'type': 
+   'EndEvent', 
+   'corners': [
+       [414, 505],
+       [459, 503],
+       [462, 549],
+       [416, 550]
+      ]
+  }
+]
+"""
+
+
+def get_next_bmpn_snapshot():
+  return bpmn_snapshot_data
+
+
+async def bpmn_snapshot_stream(websocket, path):
+  while True:
+    await websocket.send(get_next_bmpn_snapshot())
+    await asyncio.sleep(1)
+
+
+start_server = websockets.serve(bpmn_snapshot_stream, "127.0.0.1", 5678)
+
+
 async def hello(websocket, path):
-  name = await websocket.recv()
-  print(f"< {name}")
+  message = await websocket.recv()
+  print(f"Received {message}")
 
-  greeting = f"Hello {name}!"
+  await websocket.send(bpmn_snapshot_data)
+  print(f"> Send bpmn_snapshot")
 
-  await websocket.send(greeting)
-  print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "localhost", 8765)
+# start_server = websockets.serve(hello, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
